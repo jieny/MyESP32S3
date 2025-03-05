2b24e0d454dc00b9cd03623621e7a0372b37b50f
jieny <576420147@qq.com>
2025-03-05 15:28:26 +0800

    ota可以 172.18.8.132 测试ok

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,70 @@
-#include <HardwareSerial.h>
+#include <WiFi.h>
+#include <ESPmDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
 
-HardwareSerial MyHardwareSerial(2);
+const char *ssid = "Excellence";
+const char *password = "Pa22446689";
 
 void setup() {
-    delay(500);
     Serial.begin(115200);
-    MyHardwareSerial.begin(115200, SERIAL_8N1, 12, 11);
+    Serial.println("Booting");
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
+    while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+        Serial.println("Connection Failed! Rebooting...");
+        delay(5000);
+        ESP.restart();
+    }
 
-    Serial.println("能打印字？");
-    printf("hello 你好鸭");
+    // Port defaults to 3232
+     ArduinoOTA.setPort(3232);
+
+    // Hostname defaults to esp3232-[MAC]
+    // ArduinoOTA.setHostname("myesp32");
+
+    // No authentication by default
+    // ArduinoOTA.setPassword("admin");
+
+    // Password can be set with it's md5 value as well
+    // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
+    // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+
+    ArduinoOTA
+            .onStart([]() {
+                String type;
+                if (ArduinoOTA.getCommand() == U_FLASH)
+                    type = "sketch";
+                else // U_SPIFFS
+                    type = "filesystem";
+
+                // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+                Serial.println("Start updating " + type);
+            })
+            .onEnd([]() {
+                Serial.println("\nEnd");
+            })
+            .onProgress([](unsigned int progress, unsigned int total) {
+                Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+            })
+            .onError([](ota_error_t error) {
+                Serial.printf("Error[%u]: ", error);
+                if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+                else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+                else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+                else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+                else if (error == OTA_END_ERROR) Serial.println("End Failed");
+            });
+
+    ArduinoOTA.begin();
+
+    Serial.println("Ready");
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
 }
 
 void loop() {
-    delay(10000);
-    Serial.println("一直循环");
+    ArduinoOTA.handle();
+    delay(5000);
+    printf("每5秒打一次\n");
 }
